86e2dd59bb810d3e934519256d6bf01ad6e21199
vladislav <vladnovoren@gmail.com>
2022-04-04 15:45:44 +0300

    error_msgs

---
+++ include/error_msgs.hpp
@@ -0,0 +1,8 @@
+#ifndef ERROR_MSGS_HPP
+#define ERROR_MSGS_HPP
+
+static const char* const BAD_POP_MSG = "attempt to remove the last element of an empty vector";
+static const char* const BAD_INDEX_MSG = "attempt to access on vector with invalid index";
+static const char* const BAD_STATIC_OVRFLW = "attempt to use more static memory that we have";
+
+#endif /* error_msgs.hpp */
